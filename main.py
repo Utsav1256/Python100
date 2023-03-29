c1233799111129bde2f80ea684e1dec76101de47
Utsav <utsavkumar5462@gmail.com>
2023-03-29 23:40:59 +0530

    input function

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 print("Hello World!")
-print("Hello World!\nHello World!\nHello Wprld")
+print("Hello World!\nHello World!\nHello World")
 
 #Concatenating strigs -> combining two strings
 print("Hello" + " " + "Utsav")
+
+#input function
+print("Hello " + input("What is your name?") + "!")
