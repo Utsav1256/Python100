feadda606f86d1425ba01cfb69ade06b06493b03
Utsav <utsavkumar5462@gmail.com>
2023-03-30 00:13:28 +0530

    Variables

--- main.py
+++ main.py
@@ -1,11 +1,25 @@
-print("Hello World!")
-print("Hello World!\nHello World!\nHello World")
+#Variables
+name ="Utsav"
+print(name)
+name = " Muskan"
+print(name)
 
-#Concatenating strigs -> combining two strings
-print("Hello" + " " + "Utsav")
+name = input("What is your name?")
+length = len(name)
+print(length)
 
-#input function
-print("Hello " + input("What is your name?") + "!")
+# Write a program that switches the values stored in the variables a and b. 
+# # Example Input
+# a: 3
+# b: 5
+# # Example Output
+# a: 5
+# b: 3
+a = input("a : ")
+b = input("b : ")
 
-#Write a program that prints the number of characters in a user's name. 
-print(len(input("What is your name? ")))
+temp = a
+a = b
+b = temp
+print("a : " + a)
+print("b : " + b)
